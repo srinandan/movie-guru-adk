d017b575c76e7d82a0a3abb7d90005f62303b8c3
srinandan <srinandans@google.com>
2025-10-28 22:46:04 +0000

    feat: adds a2a client

--- a2atest/reasoning.py
+++ a2atest/reasoning.py
@@ -1,5 +1,6 @@
 import os
 import google.auth
+import asyncio
 import vertexai
 from google.genai import types
 
@@ -7,27 +8,20 @@ credentials, project_id = google.auth.default()
 
 PROJECT_ID = os.environ.setdefault("GOOGLE_CLOUD_PROJECT", project_id)
 LOCATION = os.environ.setdefault("GOOGLE_CLOUD_LOCATION", "us-central1")
-RESOURCE_ID = "7388282732050120704"
-RESOURCE_NAME = f"projects/{PROJECT_ID}/locations/{LOCATION}/reasoningEngines/{RESOURCE_ID}"
+RESOURCE_ID="6699794939015856128"
+REASONING_ENGINE=f"projects/{PROJECT_ID}/locations/{LOCATION}/reasoningEngines/{RESOURCE_ID}"
 
 client = vertexai.Client(
     project=PROJECT_ID,
     location=LOCATION,
-    http_options=types.HttpOptions(api_version="v1beta1")
-)
+    http_options=types.HttpOptions(api_version="v1beta1"))
 
-remote_agent = client.agent_engines.get(name=RESOURCE_NAME)
 
-response = remote_agent.handle_authenticated_agent_card()
+remote_agent = client.agent_engines.get(name=REASONING_ENGINE)
 
-print(response)
+async def main():
+    agent_card = await remote_agent.handle_authenticated_agent_card()
+    print(agent_card)
 
-message_data = {
-  "messageId": "remote-agent-message-id",
-  "role": "user",
-  "parts": [{"kind": "text", "text": "Can you recommend a few movies?"}],
-}
-
-response = remote_agent.api_client.on_message_send(**message_data)
-
-print(response)
+if __name__ == "__main__":
+    asyncio.run(main())
