822cda4533bb2d2ebd653638d048114104ffc112
srinandan <srinandans@google.com>
2026-01-01 05:49:52 +0000

    feat: chain agents

--- movie-guru-agent/app/agent.py
+++ movie-guru-agent/app/agent.py
@@ -15,6 +15,7 @@
 from google.adk.agents import SequentialAgent
 from app.subagents.recommendmovies.recommendmovies import get_recommender_agent
 from app.subagents.conversation_analysis.conversation_analysis import get_conversation_analysis_agent
+from app.subagents.formatresponse.formatresponse import get_formatresponse_agent
 
 def get_agent() -> SequentialAgent:
     """Creates and returns the movie guru agent."""
@@ -23,9 +24,10 @@ def get_agent() -> SequentialAgent:
         sub_agents=[
             get_conversation_analysis_agent(),
             get_recommender_agent(),
+#            get_formatresponse_agent(),
         ],
         description=
-        "Executes a sequence of user profile, recommendations and sentiment analysis to return a list of movies.",
+        "Executes a sequence of movie conversation analysis, movie recommendations and formatting the response.",
     )
 
 
